684865bf6e42321a67a501e3e2172471d353b635
ang10291 <krylovaangel@mail.ru>
2023-12-06 19:12:52 +0300

    has tree Constructors

---
+++ ex4_StaffCompany_constructors/Employee.java
@@ -0,0 +1,27 @@
+public class Employee {
+    private final String name;
+    private int phoneNum;
+    private String email = "не задан";
+
+    public Employee(String name, int phoneNum) {
+        this.name = name;
+        this.phoneNum = phoneNum;
+    }
+
+    public Employee(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Employee(String name, int phoneNum, String email) {
+        this.name = name;
+        this.phoneNum = phoneNum;
+        this.email = email;
+    }
+
+    public String printInfo() {
+        return  "Имя: " + name + "\n" +
+                "E-mail: " + email + "\n" +
+                "Номер: " + (phoneNum > 0 ? phoneNum : "не задан") + "\n";
+    }
+}
