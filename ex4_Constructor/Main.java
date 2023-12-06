6cf21a6f1ad5503fbf5dc92d9c6318e9022da8b3
ang10291 <krylovaangel@mail.ru>
2023-12-06 18:35:44 +0300

    Код оптимизирован

--- ex4_Constructor/Main.java
+++ ex4_Constructor/Main.java
@@ -1,21 +1,22 @@
 public class Main {
-
     public static void main(String[] args) {
-        Users misha = new Users("Misha ", 21);
-        Users dima = new Users("Dima ", 34);
-        Users katya = new Users("Katya ", 20);
+        Users misha = new Users("Misha", -2);
+        Users dima = new Users("Dima", 34);
+        Users katya = new Users("Katya", 20);
 
         printUserInfo(misha);
         printUserInfo(dima);
         printUserInfo(katya);
     }
+
     static void printUserInfo(Users users) {
-        System.out.println("User: " + users.getName() + users.getAge() + postfixGenerator(users.getAge()));
+        System.out.println(users + postfixGenerator(users.getAge()));
     }
+
     static String postfixGenerator(int age) {
-//        if (age < 0) {
-//            throw new IllegalAccessException("Возраст не может быть отрицательным");
-//        }
+        if (age < 0) {
+            return " <-- Возраст не может быть отрицательным";
+        }
         if (age % 10 == 1 && age % 100 != 11) {
             return " год";
         } else if ((age % 10 >= 2 && age % 10 <= 4) &&
