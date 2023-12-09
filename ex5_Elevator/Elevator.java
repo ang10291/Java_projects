46496472c502b75e05a6555687477cdea219ae70
ang10291 <krylovaangel@mail.ru>
2023-12-09 19:58:09 +0300

    Программа, эмулирующая работу пассажирского лифта

---
+++ ex5_Elevator/Elevator.java
@@ -0,0 +1,40 @@
+public class Elevator {
+    private int currentFloor;
+    private final int minFloor;
+    private final int maxFloor;
+
+    public Elevator(int minFloor, int maxFloor) {
+        this.minFloor = minFloor;
+        this.maxFloor = maxFloor;
+        currentFloor = 1;
+    }
+    public void moveUp() {
+        if (currentFloor < maxFloor) {
+            currentFloor++;
+        }
+    }
+    public void moveDown() {
+        if (currentFloor > minFloor) {
+            currentFloor--;
+        }
+    }
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
+
+    public void move(int floor) {
+        if (floor < minFloor || floor > maxFloor) {
+            System.out.println("Недоступный этаж.");
+            return;
+        }
+        while (floor != currentFloor) {
+            if (currentFloor < floor) {
+                moveUp();
+            } else if (currentFloor > floor) {
+                moveDown();
+            }
+            System.out.println(getCurrentFloor() + " current");
+        }
+
+    }
+}
