f279f9de0842030a41fc1dd8bb63f43d7a4dd256
ang10291 <krylovaangel@mail.ru>
2024-01-16 03:35:55 +0300

    update print

--- ex8_ATM/MoneyCalculator.java
+++ ex8_ATM/MoneyCalculator.java
@@ -3,11 +3,12 @@ package NumberAndDateExperiments_08;
 public class MoneyCalculator {
     private static final int BANKNOTES_PER_PACK = 100;
     private static final int PACKS_IN_CASSETTE = 20;
-    public static void  calculateCassetteCount(int banknoteCount) {
+    public static void calculateCassetteCount(int banknoteCount) {
         int packCount = banknoteCount / BANKNOTES_PER_PACK + (banknoteCount % BANKNOTES_PER_PACK == 0 ? 0 : 1);
         int cassetteCount = packCount / PACKS_IN_CASSETTE + (packCount % PACKS_IN_CASSETTE == 0 ? 0 : 1);
-        if (cassetteCount > 10) {
-            System.out.println("Этот банкомат заполнен");
-        } else System.out.println(cassetteCount);
+        if (packCount >= 21) {
+            System.out.println("The ATM is full");
+        } else System.out.println(cassetteCount + " cassettes has in the ATM");
     }
+
 }
