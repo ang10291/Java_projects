34ab723014fac96a970698ee8f7e299dec599855
ang10291 <krylovaangel@mail.ru>
2024-01-28 00:32:39 +0300

    code refactoring

--- ex8_containerAndBD/Container.java
+++ ex8_containerAndBD/Container.java
@@ -15,30 +15,21 @@ public class Container {
         int containerCount = boxes / BOXES_IN_CONTAINER + (boxes % BOXES_IN_CONTAINER == 0 ? 0 : 1);
         int truckCount = containerCount / CONTAINERS_IN_TRUCK + (containerCount % CONTAINERS_IN_TRUCK == 0 ? 0 : 1);
 
-        int trucks = 1;
         int containers = 1;
-        int boxCount = 1;
-
-        System.out.println("Грузовик: " + trucks);
-        System.out.println("\tКонтейнер: " + containers);
-
-        for (int i = 1; i <= boxes; i++) {
-            if (boxCount > BOXES_IN_CONTAINER) {
-                boxCount = 1;
+        int truck = 1;
+        //объяснение: в одном контейнере 27 боксов, а в одном грузовике 12 контейнеров или 324 боксов.
+        for (int box = 0; box < boxes; ) {
+            if (box % (CONTAINERS_IN_TRUCK * BOXES_IN_CONTAINER) == 0) {
+                System.out.println("Грузовик: " + truck);
+                truck++;
+            }
+            if (box % BOXES_IN_CONTAINER == 0) {
+                System.out.println("\tКонтейнер: " + containers);
                 containers++;
-                System.out.println("\tКонтейнер: " + containers);
             }
-            boxCount++;
-            if (containers > CONTAINERS_IN_TRUCK) {
-                containers = 1;
-                trucks++;
-                System.out.println("Грузовик: " + trucks);
-                System.out.println("\tКонтейнер: " + containers);
-            }
-
-            System.out.println("\t\tЯщик: " + i);
+            ++box;
+            System.out.println("\t\tЯщик: " + box);
         }
-
         System.out.println("\nНеобходимо:\n\n" +
                 "грузовиков - " + truckCount + " шт." + System.lineSeparator() +
                 "контейнеров - " + containerCount + " шт.");
